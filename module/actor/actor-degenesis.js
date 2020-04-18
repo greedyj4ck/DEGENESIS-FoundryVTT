5b68350a2a596afc9c784008013e2e4b52d29df0
Russell <russellthurman94@gmail.com>
2020-04-18 01:15:49 -0500

    Condition bars

--- module/actor/actor-degenesis.js
+++ module/actor/actor-degenesis.js
@@ -4,4 +4,34 @@
  */
 export class DegenesisActor extends Actor {
   
+    prepareData() 
+    {
+        try
+        {
+            super.prepareData();
+            const data = this.data;
+
+            if (data.data.skills.focus.value)
+                data.data.status.ego.max = (data.data.skills.focus.value + data.data.attributes[data.data.skills.focus.attribute].value) * 2
+            else if (data.data.skills.primal.value)
+                data.data.status.ego.max = (data.data.skills.primal.value + data.data.attributes[data.data.skills.primal.attribute].value) * 2
+
+            if (data.data.skills.willpower.value)
+                data.data.status.spore.max = (data.data.skills.willpower.value + data.data.attributes[data.data.skills.willpower.attribute].value) * 2
+            else if (data.data.skills.faith.value)
+                data.data.status.spore.max = (data.data.skills.faith.value + data.data.attributes[data.data.skills.faith.attribute].value) * 2
+    
+            data.data.status.fleshwounds.max = (data.data.attributes.body.value + data.data.skills.toughness.value) * 2 
+            
+            // data.data.status.ego.max = 10;
+            // data.data.status.fleshwounds.max = 10;
+            // data.data.status.spore.max =10;
+            // data.data.status.ego.max = 10;
+            
+        }
+        catch(e)
+        {
+            console.log(e);
+        }
+    }
 }
--- module/actor/actor-sheet.js
+++ module/actor/actor-sheet.js
@@ -26,6 +26,11 @@ export class DegenesisActorSheet extends ActorSheet {
     data.conceptIcon = this.actor.data.data.details.concept.value ? `systems/degenesis/icons/concept/${this.actor.data.data.details.concept.value}.svg` : "systems/degenesis/icons/blank.png";
     data.cultIcon = this.actor.data.data.details.cult.value ? `systems/degenesis/icons/cult/${this.actor.data.data.details.cult.value}.svg` : "systems/degenesis/icons/blank.png";
     data.cultureIcon = this.actor.data.data.details.culture.value ? `systems/degenesis/icons/culture/${this.actor.data.data.details.culture.value}.svg` : "systems/degenesis/icons/blank.png";
+
+    data.data.status.ego.pct = (1 - data.data.status.ego.value / data.data.status.ego.max)*100;
+    data.data.status.fleshwounds.pct = (1 - data.data.status.fleshwounds.value / data.data.status.fleshwounds.max)*100;
+    data.data.status.spore.pct = (1 - data.data.status.spore.value / data.data.status.spore.max)*100;
+    data.data.status.trauma.pct = (1 - data.data.status.trauma.value / data.data.status.trauma.max)*100;
     return data;
   }
 
@@ -36,6 +41,8 @@ export class DegenesisActorSheet extends ActorSheet {
     sheetData.concepts = DEGENESIS.concepts
     sheetData.cults = DEGENESIS.cults
     sheetData.cultures = DEGENESIS.cultures
+
+
   }
 
   /* -------------------------------------------- */
@@ -44,16 +51,13 @@ export class DegenesisActorSheet extends ActorSheet {
 	activateListeners(html) {
     super.activateListeners(html);
 
-    // Activate tabs
-    let tabs = html.find('.tabs');
-    let initial = this._sheetTab;
-    new Tabs(tabs, {
-      initial: initial,
-      callback: clicked => this._sheetTab = clicked.data("tab")
-    });
-
     // Everything below here is only needed if the sheet is editable
     if (!this.options.editable) return;
+
+    $("input[type=text]").focusin(function() {
+      $(this).select();
+    });
+
 
     // Update Inventory Item
     html.find('.item-edit').click(ev => {
