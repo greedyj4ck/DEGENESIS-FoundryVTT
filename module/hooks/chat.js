b974454d1610b2e1804e39d1286a9c6155001b8f
Russell <russellthurman94@gmail.com>
2021-02-28 17:52:05 -0600

    Support roll mode visibility for rolls

--- module/hooks/chat.js
+++ module/hooks/chat.js
@@ -21,6 +21,13 @@ export default function () {
     //   }
     // })
 
+    Hooks.on("renderChatMessage", async (app, html, msg) => {
+        // Do not display "Blind" chat cards to non-gm
+        if (html.hasClass("blind") && !game.user.isGM) {
+            html.find(".message-header").remove(); // Remove header so Foundry does not attempt to update its timestamp
+            html.html("").css("display", "none");
+        }
+    })
 
     // Activate chat listeners defined in dice-wfrp4e.js
     Hooks.on('renderChatLog', (log, html, data) => {
--- module/utility.js
+++ module/utility.js
@@ -52,4 +52,32 @@ export class DEG_Utility {
                 return key
         throw "Could not find key corresponding to " + value
     }
+
+      /**
+   * Helper function to set up chat data (set roll mode and content).
+   * 
+   * @param {String} content 
+   * @param {String} modeOverride 
+   * @param {Boolean} isRoll 
+   */
+  static chatDataSetup(content, modeOverride, isRoll = false, forceWhisper) {
+    let chatData = {
+      user: game.user._id,
+      rollMode: modeOverride || game.settings.get("core", "rollMode"),
+      content: content
+    };
+    if (isRoll)
+      chatData.sound = CONFIG.sounds.dice
+
+    if (["gmroll", "blindroll"].includes(chatData.rollMode)) chatData["whisper"] = ChatMessage.getWhisperRecipients("GM").map(u => u.id);
+    if (chatData.rollMode === "blindroll") chatData["blind"] = true;
+    else if (chatData.rollMode === "selfroll") chatData["whisper"] = [game.user];
+
+    if (forceWhisper) { // Final force !
+      chatData["speaker"] = ChatMessage.getSpeaker();
+      chatData["whisper"] = ChatMessage.getWhisperRecipients(forceWhisper);
+    }
+
+    return chatData;
+  }
 }
