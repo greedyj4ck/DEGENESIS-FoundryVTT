0db1654c3a9e0787d31b4c511a6e6e4ff55c22b8
mvissac <mvissac26@gmail.com>
2023-08-04 15:43:47 +0200

    sheet title & subtitle

--- module/hooks/handlebars.js
+++ module/hooks/handlebars.js
@@ -1,4 +1,4 @@
-import {DEGENESIS} from "../config.js"
+import { DEGENESIS } from "../config.js"
 import { ShowInventoryHeaders } from "../settings.js"
 
 export default function () {
@@ -8,16 +8,14 @@ export default function () {
             return game.user.isGM ? options.fn(this) : options.inverse(this)
         })
 
-
         Handlebars.registerHelper("isGM", function (options) {
             return game.user.isGM
         })
 
-
         Handlebars.registerHelper("config", function (key) {
             return DEGENESIS[key]
         })
-        
+
         Handlebars.registerHelper("configLookup", function (obj, key) {
             return DEGENESIS[obj][key]
         })
@@ -30,16 +28,15 @@ export default function () {
             return ShowInventoryHeaders()
         })
 
-        Handlebars.registerHelper('moreThanSixDice', function(rolls) {
-            if(rolls.length > 6){return true;}else{return false;}
-                })
-
-        Handlebars.registerHelper('isSuccess', function(result){
-            if(result ==='success'){return true;}else{return false}
+        Handlebars.registerHelper('moreThanSixDice', function (rolls) {
+            if (rolls.length > 6) { return true; } else { return false; }
         })
 
-        Handlebars.registerHelper('currentEgo', function(ego){
-            
+        Handlebars.registerHelper('isSuccess', function (result) {
+            if (result === 'success') { return true; } else { return false }
+        })
+
+        Handlebars.registerHelper('currentEgo', function (ego) {
             return (ego.max - ego.value)
         })
 
@@ -52,6 +49,41 @@ export default function () {
             }
 
             return result + '</div>';
+        })
+        Handlebars.registerHelper('drawTitle', function (title) {
+            return `<div class="header-column header-line">
+                        <div class="square little left"></div>
+                        <svg class="start-line" height="11" width="9%">
+                            <line x1="0%" y1="5" x2="88%" y2="5" style="stroke: rgb(164, 164, 164); stroke-width: 1"></line>
+                            <line x1="88%" y1="5" x2="100%" y2="11" style="stroke: rgb(164, 164, 164); stroke-width: 1"></line>
+                        </svg>
+                        <svg class="title-line middle-left" height="11" width="100%">
+                            <line x1="0" y1="11" x2="100%" y2="11" style="stroke: black; stroke-width: 1"></line>
+                        </svg>
+                        <div class="middle-name">${title}</div>
+                        <svg class="title-line middle-right" height="11" width="100%">
+                            <line x1="0" y1="11" x2="100%" y2="11" style="stroke: black; stroke-width: 1"></line>
+                        </svg>
+                        <svg class="start-line" height="11" width="9%">
+                            <line x1="0%" y1="11" x2="12%" y2="5" style="stroke: rgb(164, 164, 164); stroke-width: 1"></line>
+                            <line x1="12%" y1="5" x2="100%" y2="5" style="stroke: rgb(164, 164, 164); stroke-width: 1"></line>
+                        </svg>
+                        <div class="square little right"></div>
+                    </div>
+                    <div class="break"></div>`;
+        })
+        Handlebars.registerHelper('drawSubtitle', function (title) {
+            return `<div class="body-column body-line">
+                        <div class="square little left"></div>
+                        <svg class="title-line middle-left" height="11" width="100%">
+                            <line x1="0" y1="11" x2="100%" y2="11" style="stroke:black;stroke-width:1"></line>
+                        </svg>
+                        <div class="middle-name">${title}</div>
+                        <svg class="title-line middle-right" height="11" width="100%">
+                            <line x1="0" y1="11" x2="100%" y2="11" style="stroke:black;stroke-width:1"></line>
+                        </svg>
+                        <div class="square little right"></div>
+                    </div>`;
         })
         Handlebars.registerHelper('drawImage', (item) => `<div class="img-frame"><img class="profile-img" src="${item.img}" data-edit="img" title="${item.name}" height="100" width="100" /></div>`)
     })
